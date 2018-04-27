a560a99f31f13e77135313e3d2d63bed1e1d4460
kostyaverstka <33493899+kostyaverstka@users.noreply.github.com>
2018-04-27 08:17:52 +0300

    update

---
+++ hostel/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $("header").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+	$('.fotorama').fotorama({
+	  nav: 'thumbs'
+	});
+});
